78ec2df9a60c41571a60ce6e6bec4a8b794542db
vazquezjose <60374151+vazquezjose@users.noreply.github.com>
2022-04-20 20:50:48 -0500

    Práctica 3

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -9,4 +9,31 @@ export class Tab1Page {
 
   constructor() {}
 
+  alumnos = [
+    {
+      nombre: 'Christian',
+      apellido: 'Ramirez',
+      matricula: '1234567'
+    },
+    {
+      nombre: 'Aaylin',
+      apellido: 'Demetci',
+      matricula: '1234567'
+    },
+    {
+      nombre: 'Brian',
+      apellido: 'Esquivel',
+      matricula: '1234567'
+    },
+    {
+      nombre: 'Diego',
+      apellido: 'Jasso',
+      matricula: '1234567'
+    },
+    {
+      nombre: 'Jose',
+      apellido: 'Vazquez',
+      matricula: '1894608'
+    }
+  ];
 }
