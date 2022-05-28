8909ca5795903367b5561eec30ff607d3d743fbc
vazquezjose <60374151+vazquezjose@users.noreply.github.com>
2022-05-28 03:57:11 -0500

    Práctica 10 Parte 1 - Construcción del proyecto

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 import { Tab2PageRoutingModule } from './tab2-routing.module';
 
 import { AlumnosComponentModule } from '../alumnos/alumnos.component.module';
@@ -15,7 +14,6 @@ import { AlumnosComponentModule } from '../alumnos/alumnos.component.module';
 		CommonModule,
 		FormsModule,
 		AlumnosComponentModule,
-		ExploreContainerComponentModule,
 		Tab2PageRoutingModule
 	],
 	declarations: [Tab2Page]
